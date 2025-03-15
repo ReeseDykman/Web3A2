2317451bc4aad57e7ce551150977fd1202eb1b8e
ReeseDykman <reesedykman@gmail.com>
2025-03-15 15:51:47 -0600

    creating vite app and importing tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+//import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+      <h1 className="text-3xl font-bold text-yellow-600 m-4">
+        Hello world!
+      </h1>
+  )
+}
+
+export default App
