160fa2de186aba61d8aa7d66d8e74b3164ce2deb
Christopher Nottingham <91142333+Christopher-Nottingham@users.noreply.github.com>
2025-03-23 21:10:57 -0600

    Adding the GenreView to the routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login.jsx';
 import Navbar from './components/Header.jsx';
+import GenreView from './components/GenreView.jsx';
+
 
 
 function App() {
@@ -16,13 +18,14 @@ function App() {
 
   return (
     <main className="bg-gray-100 max-w-screen min-h-screen">
- {loggedIn && <Navbar />}      
+     
+    {loggedIn && <Navbar />}      
       <Routes>
         <Route path="/" element={<Navigate to="/login"/>}/>
         <Route path="/login" element={loggedIn ? <Navigate to="/paintings"/> : <Login handleLogin={handleLogin} />}/>
         <Route path="/galleries" element={loggedIn ? (<h1 className="text-4xl text-center font-bold p-8">Galleries</h1>) : (<Navigate to="/login" />)}/>
         <Route path="/artists" element={loggedIn ? (<h1 className="text-4xl text-center font-bold p-8">Artists</h1>) : (<Navigate to="/login" />)}/>
-        <Route path="/genres" element={loggedIn ? (<h1 className="text-4xl text-center font-bold p-8">Genres</h1>) : (<Navigate to="/login" />)}/>
+        <Route path="/genres" element={loggedIn ? <GenreView /> : <Navigate to="/login" />} />        
         <Route path="/paintings" element={loggedIn ? (<h1 className="text-4xl text-center font-bold p-8">Paintings</h1>) : (<Navigate to="/login" />)}/>
         <Route path="*" element={<Navigate to="/login" />}/>
       </Routes>
