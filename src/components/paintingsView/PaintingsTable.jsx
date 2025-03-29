136f00922c5400847b353feb84263ddf91d648d4
ReeseDykman <reesedykman@gmail.com>
2025-03-29 16:44:02 -0600

    started sort

--- src/components/paintingsView/PaintingsTable.jsx
+++ src/components/paintingsView/PaintingsTable.jsx
@@ -1,18 +1,28 @@
 import PaintingsTableRow from "./PaintingsTableRow.jsx";
 
 const PaintingsTable = (props) => {
+
+    const getOrder = (field) => {
+        if(props.order.field === field && props.order.value === "asc") {
+            return "↑"
+        } else if (props.order.field === field && props.order.value === "desc") {
+            return "↓"
+        }
+        return ""
+    }
+
     return (
         <div className="h-full overflow-y-auto overflow-x-auto">
             <table className="w-full border-collapse border border-gray-300">
                 <thead className="bg-gray-200">
                     <tr>
-                        <th className="p-5 text-center border border-gray-300">Thumbnail</th>
-                        <th className="p-5 text-center border border-gray-300">Title</th>
-                        <th className="p-5 text-center border border-gray-300">Artist</th>
-                        <th className="p-5 text-center border border-gray-300">Year</th>
-                        <th className="p-5 text-center border border-gray-300">Gallery</th>
-                        <th className="p-5 text-center border border-gray-300">Medium</th>
-                        <th className="p-5 text-center border border-gray-300">Dimensions</th>
+                        <th className="p-5 text-center border border-gray-300">Thumbnail </th>
+                        <th className="p-5 text-center border border-gray-300">Title {getOrder("Title")} </th>
+                        <th className="p-5 text-center border border-gray-300">Artist {getOrder("Artist")} </th>
+                        <th className="p-5 text-center border border-gray-300">Gallery {getOrder("Gallery")} </th>
+                        <th className="p-5 text-center border border-gray-300">Year {getOrder("Year")} </th>
+                        <th className="p-5 text-center border border-gray-300">Medium </th>
+                        <th className="p-5 text-center border border-gray-300">Dimensions </th>
                     </tr>
                 </thead>
                 <tbody className="bg-white">
--- src/components/paintingsView/PaintingsView.jsx
+++ src/components/paintingsView/PaintingsView.jsx
@@ -6,10 +6,23 @@ import { useContext, useState } from "react";
 const PaintingsView = () => {
     const { paintings } = useContext(PaintingsContext);
     const [filteredPaintings, setFilteredPaintings] = useState(paintings);
+    const [order, setOrder] = useState({field : "Title", value : "asc"});
 
     const handleFilter = (filter) => {
         // Filtering logic here
     };
+
+    const handleOrder = ({field, value}) => {
+        setOrder({[field]: value});
+        const sortedPaintings = [...filteredPaintings].sort((a, b) => {
+            const aValue = a[field] || 0;
+            const bValue = b[field] || 0;
+            if (aValue < bValue) return value === "asc" ? -1 : 1;
+            if (aValue > bValue) return value === "asc" ? 1 : -1;
+            return 0;
+        });
+        setFilteredPaintings(sortedPaintings);
+    }
 
     return (
         <section className="w-full h-full mx-auto p-4 flex flex-col md:flex-row gap-4">
@@ -20,7 +33,7 @@ const PaintingsView = () => {
 
             {/* Paintings List */}
             <div className="flex-1 bg-gray-100 shadow-md rounded p-4 h-165 overflow-hidden">
-                <PaintingsTable paintings={filteredPaintings} />
+                <PaintingsTable paintings={filteredPaintings} handleOrder={handleOrder} order={order}/>
             </div>
         </section>
     );
