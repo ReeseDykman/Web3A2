475f5608539705460e3970879aea1c65e1bac80f
ReeseDykman <reesedykman@gmail.com>
2025-04-03 00:36:54 -0600

    formatting changes

--- src/components/artistsView/ArtistListItem.jsx
+++ src/components/artistsView/ArtistListItem.jsx
@@ -1,22 +1,23 @@
-
 const ArtistListItem = (props) => {
-
     const artist = props.artist;
     const index = props.index;
-    
-    return(
+
+    return (
         <li
             key={artist.artistId ? `${artist.artistId}Li` : `${index}Li`}
             className="cursor-pointer p-3 bg-white rounded shadow hover:bg-gray-100 hover:shadow-md transition-all"
             onClick={() => {
                 props.handleArtistClick(artist);
-            }}>
-
-            <span key={artist.artistId ? `${artist.artistId}Span` : `${index}Span`} className="text-lg font-medium text-gray-800">
+            }}
+        >
+            <span
+                key={artist.artistId ? `${artist.artistId}Span` : `${index}Span`}
+                className="text-lg font-medium text-gray-800"
+            >
                 {`${artist.firstName} ${artist.lastName}`}
             </span>
         </li>
     );
-}
+};
 
 export default ArtistListItem;
--- src/components/artistsView/artistsView.jsx
+++ src/components/artistsView/artistsView.jsx
@@ -1,13 +1,11 @@
 import PaintingsTable from "../paintingsView/PaintingsTable";
 import ArtistsList from "./ArtistsList";
 import ArtistCard from "./ArtistCard.jsx";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { PaintingsContext } from "../../App";
 import filter from "../../scripts/filterFactory";
-import { useState } from "react";
 
 const ArtistsView = () => {
-
     const { paintings } = useContext(PaintingsContext);
     const [artistPaintings, setArtistPaintings] = useState(paintings);
     const [selectedArtist, setSelectedArtist] = useState(null);
@@ -16,7 +14,7 @@ const ArtistsView = () => {
     const handleSort = ({ field, value }, paintings) => {
         setSort(new filter(field, value));
 
-        //Returns the proper field in the json
+        // Returns the proper field in the JSON
         const fieldMapping = {
             title: (painting) => painting.title,
             artist: (painting) => `${painting.Artists.firstName} ${painting.Artists.lastName}`,
@@ -31,21 +29,25 @@ const ArtistsView = () => {
         const sortedPaintings = paintings.sort((a, b) => {
             let aValue, bValue;
 
-            //get the function for the field and pass the painting to it
+            // Get the function for the field and pass the painting to it
             aValue = fieldMapping[field.toLowerCase()](a) || "";
             bValue = fieldMapping[field.toLowerCase()](b) || "";
 
             // Years are numbers, so we need to handle them differently but we can compare them directly
-            //if ascending, subtract a from b, if descending, subtract b from a
             if (typeof aValue === "number" && typeof bValue === "number") {
                 return value === "asc" ? bValue - aValue : aValue - bValue;
             }
 
-            //if a is greater than b, return 1, if a is less than b, return -1, else return 0
-            //the return value is inverted if descending
-            if (aValue.toLowerCase().trim().replace(/ /g, "") < bValue.toLowerCase().trim().replace(/ /g, "")) {
+            // Compare strings
+            if (
+                aValue.toLowerCase().trim().replace(/ /g, "") <
+                bValue.toLowerCase().trim().replace(/ /g, "")
+            ) {
                 return value === "asc" ? -1 : 1;
-            } else if (aValue.toLowerCase().replace(/ /g, "") > bValue.toLowerCase().trim().replace(/ /g, "")) {
+            } else if (
+                aValue.toLowerCase().trim().replace(/ /g, "") >
+                bValue.toLowerCase().trim().replace(/ /g, "")
+            ) {
                 return value === "asc" ? 1 : -1;
             } else {
                 return 0;
@@ -57,29 +59,36 @@ const ArtistsView = () => {
 
     const handleArtistClick = (artist) => {
         setSelectedArtist(artist);
-        const toFilter = [...paintings]
-        const filteredPaintings = toFilter.filter((painting) => painting.Artists.artistId === artist.artistId);
+        const toFilter = [...paintings];
+        const filteredPaintings = toFilter.filter(
+            (painting) => painting.Artists.artistId === artist.artistId
+        );
         handleSort(sort, filteredPaintings);
-    }
+    };
 
     const handleBackButton = () => {
         setSelectedArtist(null);
         handleSort(sort, [...paintings]);
-    }
-
+    };
 
     return (
         <section className="w-full h-full mx-auto p-4 flex flex-col md:flex-row gap-4 justify-between">
+            <ArtistsList handleArtistClick={handleArtistClick} />
 
-            <ArtistsList handleArtistClick={handleArtistClick}/>
-
-            <ArtistCard selectedArtist={selectedArtist} handleBackButton={handleBackButton}/>
+            <ArtistCard
+                selectedArtist={selectedArtist}
+                handleBackButton={handleBackButton}
+            />
 
             <div className="flex-5 bg-gray-100 shadow-md rounded p-4 h-167 overflow-hidden">
-                <PaintingsTable paintings={artistPaintings} handleSort={handleSort} sort={sort} />
+                <PaintingsTable
+                    paintings={artistPaintings}
+                    handleSort={handleSort}
+                    sort={sort}
+                />
             </div>
         </section>
     );
-}
+};
 
 export default ArtistsView;
