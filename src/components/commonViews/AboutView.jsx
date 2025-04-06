717c55ef0071c5a0321650880c8625d564233d89
Christopher Nottingham <91142333+Christopher-Nottingham@users.noreply.github.com>
2025-04-06 12:45:11 -0600

    more styling

--- src/components/commonViews/AboutView.jsx
+++ src/components/commonViews/AboutView.jsx
@@ -5,49 +5,72 @@
 const AboutView = () => {
     return (
         // The overall container for the about section
-        <div className=" bg-gray-400 items-center justify-center text-black m-6 p-6 rounded-lg ">
+        <div className=" bg-green-700 items-center justify-center text-black m-6 p-6 rounded-lg ">
+
+            <div className=" bg-green-100 rounded-lg p-4 h-[80vh] overflow-auto">
 
 
-            {/* The title of the section in large bold font */}
-            <h1 className="text-2xl text-center font-bold">About the App</h1>
+                {/* The title of the section in large bold font */}
+                <h1 className="text-2xl text-center font-bold m-6 ">Exploring the world of art in the comfort of your home</h1>
 
-            {/* The the tabbed overall description */}
-            <p> &nbsp; This app was made to help assist art enthusiasts
-                find detailed information about genres of art, galleries,
-                and paintings all around the world.
+                <h2 className="text-lg text-center font-bold m-6 ">About the app</h2>
 
-                Made in a senior level web development class, this application was built using React, Leaflet and Supabase.
-                To allow for nativgation around the app, React Router was used.
-                Context management was used to limit the amount of props being passed around the app. In order to update the
-                page from a user query,
-                a colmbination of both state and effect management React API's were used.
-            </p>
-            {/* Forcing a new line */}
-            <br />
+                {/* The the tabbed overall description */}
+                <div className="m-2">
 
-            <p className="test-bold">We hope you enjoy using this app as much as we enjoyed making it!</p>
-            <br />
+                    <p className="m-2">
+                        &nbsp; This app was made to help assist art enthusiasts find detailed information about genres of art, galleries, and paintings all around the world.
 
-            <h2 className="text-xl text-center font-semibold ">Links to code used: </h2>
+                        <br />
+                        <br />
 
-            <h3>Page: Galleries - Gallery Info</h3>
-            <p><a target="_blank" href="https://react-leaflet.js.org/docs/start-setup/">Assistance with Leaflet - Official documentation</a></p>
+                        &nbsp; Made in a senior-level web development class, this application was built using <strong>React</strong>, <strong>Leaflet</strong> and <strong>Supabase</strong>.
 
-            <p>
-                <a target="_blank" href="https://www.youtube.com/watch?v=jD6813wGdBA"> YOUTUBE - Alejandro AO - Software & Ai :  React Leaflet Tutorial for Beginners (2023) </a>
-            </p>
-            <br />
-            <h3>Page: Paintings - Paintings Modal </h3>
-            <h3>
-                <p> <a target="_blank" href="https://www.youtube.com/watch?v=dEGbXY-8YtU&ab_channel=YourCodeLab">YOUTUBE - YOUR CODE LAB : Build a Modal Component purely in ReactJS and TailwindCSS </a></p>
-            </h3>
-            <br />
+                        React is the library used to build the UI, Leaflet is used to display the gallery on an interactive map, and Supabase is used as the backend
+                        database to store and retrieve all relevant data.
 
-            {/* Providing the viewers with the developers who made the SPA */}
-            <h2 className="text-xl text-center font-semibold ">Developers</h2>
-            <p>Reese Dykman:<a href="mailto:rdykm626@mtroyal.ca">rdykm626@mtroyal.ca</a></p>
-            <p>Christopher Nottingham:<a href="mailto:cnott729@mtroyal.ca">cnott729@mtroyal.ca</a></p>
-        </div >
+
+                        To allow for navigation around the app, React Router was used to enable the single-paged application effect.
+
+                        Furthermore, the useContext hook was used to limit the number of props being passed around the app. To update the page from a user query, a combination of the
+                        useState (save variables in a JSX file) and useEffect (do something if the state changed) React hooks were used. Lastly, to ensure responsiveness and save user
+                        favourites, local storage was implemented to ensure the user could access their favourites later and to reduce the number of pulls for the data from Supabase.
+                    </p>
+                </div>
+
+
+
+
+                {/* Code attribution */}
+
+                <h2 className="text-lg text-center font-semibold m-6 ">Links to code used: </h2>
+
+                <h3 className="m-2 font-semibold" >Page: Galleries - Gallery Info</h3>
+                <p className="m-2" ><a className="hover:text-blue-700" target="_blank" href="https://react-leaflet.js.org/docs/start-setup/">&nbsp; Assistance with Leaflet - Official documentation</a></p>
+
+                <p className="m-2" >
+                    <a className="hover:text-blue-700" target="_blank" href="https://www.youtube.com/watch?v=jD6813wGdBA">&nbsp; YOUTUBE - Alejandro AO - Software & Ai :  React Leaflet Tutorial for Beginners (2023) </a>
+                </p>
+
+                <h3 className="m-2 font-semibold">Page: Paintings - Paintings Modal </h3>
+
+                <p className="m-2"> <a className="hover:text-blue-700" target="_blank" href="https://www.youtube.com/watch?v=dEGbXY-8YtU&ab_channel=YourCodeLab">&nbsp; YOUTUBE - YOUR CODE LAB : Build a Modal Component purely in ReactJS and TailwindCSS </a></p>
+
+
+
+                {/* Providing the viewers with the developers who made the SPA */}
+                <h2 className="text-lg text-center font-semibold m-6 ">Developers</h2>
+
+                <p className="m-2">Reese Dykman:<a className="hover:text-blue-700" href="mailto:rdykm626@mtroyal.ca"> rdykm626@mtroyal.ca</a></p>
+
+                <p className="m-2">Christopher Nottingham:<a className="hover:text-blue-700" href="mailto:cnott729@mtroyal.ca"> cnott729@mtroyal.ca</a></p>
+
+                <p className="text-lg font-semibold text-center m-2 ">We hope you enjoy using this app as much as we enjoyed making it!</p>
+
+                {/* Providing the user to a hyperlinked header to the github repo */}
+                <h2 className="text-lg text-center font-semibold m-6 hover:text-blue-700 "><a href="https://github.com/ReeseDykman/Web3A2" target="_blank" rel="noopener noreferrer">Github Repository</a></h2>
+            </div >
+        </div>
 
     );
 }
